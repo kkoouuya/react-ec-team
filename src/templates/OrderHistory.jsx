4618e195e782311e23705f90e242f1ebca173a8c
goldst0 <goldst0912@gmail.com>
2021-05-28 08:07:58 +0900

    画面

--- src/templates/OrderHistory.jsx
+++ src/templates/OrderHistory.jsx
@@ -1,17 +1,20 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
-import Button from '@material-ui/core/Button';
-import Icon from '@material-ui/core/Icon';
-// import LocalGroceryStoreIcon from '@material-ui/icons/LocalGroceryStore';
-import RemoveShoppingCartIcon from '@material-ui/icons/RemoveShoppingCart';
-import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
+import Button from "@material-ui/core/Button";
+import AddShoppingCartIcon from "@material-ui/icons/AddShoppingCart";
+import CancelIcon from "@material-ui/icons/Cancel";
+import HistoryIcon from "@material-ui/icons/History";
+import IconButton from "@material-ui/core/IconButton";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import Link from "@material-ui/core/Link";
 
 const useStyles = makeStyles({
   table: {
@@ -19,74 +22,131 @@ const useStyles = makeStyles({
   },
 });
 
-function createItem(delivery,pizza, topping,quantity) {
-  return { delivery, pizza, topping,quantity };
-}
-const items = [
-  createItem('https://static.pizzahut.jp/jp/menu/single/desktop_thumbnail_63e36f58-0e6a-43cf-bf5d-721f0cc6bb98.jpg', 'hawaianパラダイス', 'タコス','1'),
-  createItem('https://static.pizzahut.jp/jp/menu/single/desktop_thumbnail_63e36f58-0e6a-43cf-bf5d-721f0cc6bb98.jpg', 'hawaianパラダイス', 'タコス','1'),
-  createItem('https://static.pizzahut.jp/jp/menu/single/desktop_thumbnail_63e36f58-0e6a-43cf-bf5d-721f0cc6bb98.jpg', 'hawaianパラダイス', 'タコス','1'),
+const orders = [
+  {
+    id: "",
+    orderDate: "2021-5-21",
+    item_imagePath:
+      "https://static.pizzahut.jp/jp/menu/single/desktop_thumbnail_63e36f58-0e6a-43cf-bf5d-721f0cc6bb98.jpg",
+    item_name: "hawaianパラダイス",
+    topping_name: "タコス",
+    item_num: "1",
+    status: "1",
+  },
+  {
+    id: "",
+    orderDate: "2021-6-2",
+    item_imagePath:
+      "https://static.pizzahut.jp/jp/menu/single/desktop_thumbnail_63e36f58-0e6a-43cf-bf5d-721f0cc6bb98.jpg",
+    item_name: "hawaianパラダイス",
+    topping_name: "タコス",
+    item_num: "1",
+    status: "1",
+  },
+  {
+    id: "",
+    orderDate: "2021-8-2",
+    item_imagePath:
+      "https://static.pizzahut.jp/jp/menu/single/desktop_thumbnail_63e36f58-0e6a-43cf-bf5d-721f0cc6bb98.jpg",
+    item_name: "hawaianパラダイス",
+    topping_name: "タコス",
+    item_num: "1",
+    status: "1",
+  },
 ];
 
 const OrderHistory = () => {
+  // const button = {
+  //   disabled: true,
+  // };
+  const button_label = () => {
+    return {
+      // disabled ?'キャンセル':'キャンセル済み'
+      button_label: "キャンセル",
+    };
+  };
+  const cancel = () => {
+    return { label: false };
+  };
   return (
     <React.Fragment>
-    <TableContainer >
-<Table aria-label="spanning table" >
-  <TableHead>
-    <TableRow>
-      <TableCell colSpan="4">
-        <h1 align="center">注文履歴</h1>
-      </TableCell>
-    </TableRow>
-    <TableRow>
-      <TableCell align="left">配達日</TableCell>
-      <TableCell align="left">商品名</TableCell>
-      <TableCell align="left">トッピング</TableCell>
-      <TableCell align="left">数量</TableCell>
-    </TableRow>
-  </TableHead>
-  <TableBody>
-    {items.date}
-{items.map((item)=>(
-  <TableRow key={item.delivery}>
-    <TableCell>{item.delivery}</TableCell>
-    <TableCell>{item.pizza}</TableCell>
-    <TableCell>{item.topping}</TableCell>
-    <TableCell>{item.quantity}</TableCell>
-    <TableCell>
-    <Button
-        variant="contained"
-        color="secondary"
-        startIcon={<ShoppingCartIcon />}
-      >
-        もう一度注文する
-      </Button>
-    </TableCell>
-  </TableRow>
-))}
-<TableRow>
-  <TableCell colSpan={4} align="right">合計金額：{}円
-  </TableCell>
-  <TableCell>
-  <Button
-        variant="contained"
-        color="primary"
-        endIcon={<RemoveShoppingCartIcon />}
-      >
-        キャンセル
-      </Button>
-  </TableCell>
-</TableRow>
-
-  </TableBody>
-</Table>
-
-    </TableContainer>
-    {/* <Button variant="contained" align="center" style={{color:"white"}}>
+      <Card>
+        <h1 align="center">
+          <HistoryIcon style={{ fontSize: 20 }} color="primary" />
+          注文履歴
+        </h1>
+        <TableContainer>
+          <Table aria-label="spanning table">
+            <TableHead>
+              {/* <TableRow>
+                <TableCell colSpan="4">
+                  <h1 align="center">
+                    <HistoryIcon style={{ fontSize: 20 }} color="primary" />
+                    注文履歴
+                  </h1>
+                </TableCell>
+              </TableRow> */}
+              <TableRow>
+                <TableCell align="center">配達日</TableCell>
+                <TableCell align="center">商品名</TableCell>
+                <TableCell align="center">トッピング</TableCell>
+                <TableCell align="center">数量</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {orders.length > 0 &&
+                orders.map((order) => (
+                  <TableRow order={order} key={order.id}>
+                    <TableCell>
+                      <CardContent align="center">
+                        <h3> {order.orderDate}に配達</h3>
+                        <img
+                          src={order.item_imagePath}
+                          alt="アイコン"
+                          height="150px"
+                          align="center"
+                        />
+                      </CardContent>
+                    </TableCell>
+                    <TableCell>
+                      <Link>{order.item_name}</Link>
+                    </TableCell>
+                    <TableCell>{order.topping_name}</TableCell>
+                    <TableCell>{order.item_num}</TableCell>
+                    <TableCell>
+                      <Button
+                        variant="contained"
+                        color="primary"
+                        startIcon={<AddShoppingCartIcon />}
+                      >
+                        もう一度注文する
+                      </Button>
+                    </TableCell>
+                  </TableRow>
+                ))}
+              <TableRow>
+                <TableCell colSpan={4} align="right">
+                  合計金額：{}円
+                </TableCell>
+                <TableCell>
+                  <Button
+                    variant="contained"
+                    startIcon={<CancelIcon />}
+                    // disabled={orders[0].status ? "9" : "1"}
+                    onClick={cancel}
+                  >
+                    キャンセル
+                  </Button>
+                </TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </TableContainer>
+        {/* <Button variant="contained" align="center" style={{color:"white"}}>
       メニューに戻る
       </Button> */}
-</React.Fragment>
+      </Card>
+    </React.Fragment>
   );
 };
 
