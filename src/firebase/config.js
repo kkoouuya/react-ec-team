a99897f07db8cd459aa9b82c4c1ed322389de7eb
WataruTamura <wataru.tamura@rakus-partners.co.jp>
2021-05-27 17:47:31 +0900

    商品一覧画面

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-  apiKey: 'AIzaSyDbchXVes2qAVjNQHXe7ygDUnTlBAkCBb8',
-  authDomain: 'my-react-project-4d865.firebaseapp.com',
-  projectId: 'my-react-project-4d865',
-  storageBucket: 'my-react-project-4d865.appspot.com',
-  messagingSenderId: '891412353539',
-  appId: '1:891412353539:web:3c4df85f02fe36da043436',
-  measurementId: 'G-K4GC33NFM9',
+  apiKey: 'AIzaSyCNdPYzGv46J-g_FFTt_EqNfV4O0PwOFZY',
+  authDomain: 'react-teame.firebaseapp.com',
+  projectId: 'react-teame',
+  storageBucket: 'react-teame.appspot.com',
+  messagingSenderId: '1008822676103',
+  appId: '1:1008822676103:web:0e679d3eb66615b51ca622',
+  measurementId: 'G-G9049Q1QLR',
 };
